4da3e0101abc9fc823a4dd8b8f47a258e97d8708
austinkret <austinkret@gmail.com>
2021-04-12 21:09:11 +0000

    linting adjustments to the file

--- express_server.js
+++ express_server.js
@@ -29,7 +29,7 @@ app.get('/urls', (request, response) => {
 app.get('/urls/:shortURL', (request, response) => {
   const templateVars = { shortURL: request.params.shortURL, longURL: request.params.longURL };
   response.render('urls_show', templateVars);
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
