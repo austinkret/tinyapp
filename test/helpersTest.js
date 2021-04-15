79bb23c2389ea7b710ffe4c5914d90b9ccddffa9
austinkret <austinkret@gmail.com>
2021-04-15 23:22:42 +0000

    mocha chai tests passing for first function

---
+++ test/helpersTest.js
@@ -0,0 +1,31 @@
+const { assert } = require('chai');
+
+const { findUserByEmail } = require('../helper');
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('findUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = findUserByEmail("user@example.com", users);
+    const userid = user.id;
+    const expectedOutput = "userRandomID";
+    assert.equal(userid, expectedOutput);
+  });
+
+  it('should return a undefined if the email does not exist', function() {
+    const user = findUserByEmail("notanemail@gexample.com", users);
+    const expectedOutput = undefined;
+    assert.equal(user, expectedOutput);
+  });
+});
